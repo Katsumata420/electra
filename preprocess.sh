720ec9cd21a0e0891c148410aa888c5c5ba54627
Katsumata420 <katsumata-satoru@ed.tmu.ac.jp>
2021-06-30 19:43:13 +0900

    fix preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -1,9 +1,9 @@
 python build_pretraining_dataset.py \
-  --corpus_dir ./data \
-  --vocab_file ./vocab.txt \
-  --output_dir ./electra_output \
-  --max_seq_length 128 \
-  --num_process 1 \
-  --blanks_separete_docs True \
-  --do_lower_case True
+  --corpus-dir ./data \
+  --vocab-file ./vocab.txt \
+  --output-dir ./electra_data_bin \
+  --max-seq-length 128 \
+  --num-process 1 \
+  --blanks-separate-docs True \
+  --do-lower-case
 
